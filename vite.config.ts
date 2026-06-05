d4a86caf5a856249f99386276a2aa43b6e05c2f1
Gise <81759759+gpsanchezr@users.noreply.github.com>
2026-06-04 21:37:47 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   server: { port: 3000, open: true },
   build: {
     target: 'esnext',
+    chunkSizeWarningLimit: 1600, // <--- Aquí está el ajuste
     rollupOptions: {
       output: {
         manualChunks: {
